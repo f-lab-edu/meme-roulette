88425a3ff92c86abeca15e24cb313ae0c364f313
Minju <minju1009@gmail.com>
2023-04-10 10:39:25 +0900

    feat: App.js 추가

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import routes from './router';
+import { on } from './helpers';
+import HeaderView from './views/HeaderView';
+import HomeView from './views/HomeView/HomeView';
+
+class App {
+  constructor($body) {
+    this.$body = $body;
+    this.$main = document.createElement('main');
+    this.renderHeader();
+    this.render();
+  }
+
+  renderHeader() {
+    const header = new HeaderView(this.$body);
+    header.render();
+  }
+
+  render() {
+    this.$body.appendChild(this.$main);
+    this.navigate('/');
+    on(this.$body, '@navigate', event => this.navigate(event.detail.url));
+  }
+
+  navigate(url) {
+    const page = routes.find(route => route.path === url).view;
+    page(this.$main);
+  }
+}
+export default App;
