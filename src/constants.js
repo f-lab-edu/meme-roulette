47a1c637e2b8d1fd0b9b9c7bb3138a7a4ed5a593
minju1009 <minju1009@gmail.com>
2023-04-16 23:09:56 +0900

    refactor: 구조변경을 위해 사용하지 않는 파일 삭제

--- src/constants.js
+++
@@ -1,5 +0,0 @@
-export const mainContents = [
-  { title: 'My Lucky Meme', koreanTitle: '오늘의 내 짤 뽑기', path: '/random' },
-  { title: 'Trending', koreanTitle: '요즘 유행하는 짤', path: '/trending' },
-  { title: 'Search Memes', koreanTitle: '검색어로 짤 찾기', path: '/search' },
-];
--- src/controllers/HomeController.js
+++
@@ -1,9 +0,0 @@
-export default class HomeController {
-  constructor(store, { mainView }) {
-    this.mainView = mainView;
-    // this.render();
-  }
-  // render() {
-  //   this.mainView.render();
-  // }
-}
