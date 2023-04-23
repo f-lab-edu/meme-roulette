1c0c919889a1efe4292b321cb3c132507f9c7ee7
Minju Kim <90900882+minju1009@users.noreply.github.com>
2023-04-23 13:39:10 +0900

    Revert "Feature/home"

--- src/pages/Home/index.js
+++
@@ -1,5 +0,0 @@
-import HomeController from './HomeController';
-import HomeView from './HomeView';
-import HomeModel from './HomeModel';
-
-export { HomeController, HomeView, HomeModel };
--- src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-export function emit(target, eventName, detail) {
-  const event = new CustomEvent(eventName, { detail });
-  target.dispatchEvent(event);
-}
-
-export function navigate(url) {
-  window.history.pushState('', '', url);
-  emit(document.querySelector('#root'), '@navigate', { url });
-}
